1fb397b300470c8f860ce0f9a2a45d1a43ee36b5
clides <daniel168.guo@gmail.com>
2025-05-16 11:53:23 -0400

    updated integration with new fields

--- rerankers/models/rankllm_ranker.py
+++ rerankers/models/rankllm_ranker.py
@@ -22,9 +22,14 @@ class RankLLMRanker(BaseRanker):
         window_size: int = 20,
         context_size: int = 4096,
         prompt_mode: PromptMode = PromptMode.RANK_GPT,
+        num_few_shot_examples: int = 0,
+        few_shot_file: Optional[str] = None,
+        num_gpus: int = 1,
+        variable_passages: bool = False,
+        use_logits: bool = False,
+        use_alpha: bool = False,
         stride: int = 10,
         use_azure_openai: bool = False,
-        
     ) -> "RankLLMRanker":
         self.api_key = api_key
         self.model = model
@@ -35,6 +40,12 @@ class RankLLMRanker(BaseRanker):
         self.window_size = window_size
         self.context_size = context_size
         self.prompt_mode = prompt_mode
+        self.num_few_shot_examples = num_few_shot_examples
+        self.few_shot_file = few_shot_file
+        self.num_gpus = num_gpus
+        self.variable_passages = variable_passages
+        self.use_logits = use_logits
+        self.use_alpha = use_alpha
         self.stride = stride
         self.use_azure_openai = use_azure_openai
         
@@ -43,12 +54,19 @@ class RankLLMRanker(BaseRanker):
             "default_model_coordinator": None,
             "context_size": self.context_size,
             "prompt_mode": self.prompt_mode,
+            "num_gpus": self.num_gpus,
+            "use_logits": self.use_logits,
+            "use_alpha": self.use_alpha,
+            "num_few_shot_examples": self.num_few_shot_examples,
+            "few_shot_file": self.few_shot_file,
+            "variable_passages": self.variable_passages,
             "interactive": False,
             "window_size": self.window_size,
             "stride": self.stride,
             "use_azure_openai": self.use_azure_openai,
         }
-        self.model_coordinator = rankllm_Reranker.create_model_coordinator(**kwargs)
+        model_coordinator = rankllm_Reranker.create_model_coordinator(**kwargs)
+        self.reranker = rankllm_Reranker(model_coordinator)
 
     def rank(
         self,
@@ -61,7 +79,6 @@ class RankLLMRanker(BaseRanker):
     ) -> RankedResults:
         docs = prep_docs(docs, doc_ids, metadata)
 
-        reranker = rankllm_Reranker(self.model_coordinator)
         request = Request(
             query=Query(text=query, qid=1),
             candidates=[
@@ -70,7 +87,7 @@ class RankLLMRanker(BaseRanker):
             ],
         )
 
-        rankllm_results = reranker.rerank(
+        rankllm_results = self.reranker.rerank(
             request,
             rank_end=len(docs) if rank_end == 0 else rank_end,
             window_size=min(20, len(docs)),
